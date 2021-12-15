13b04e793d7bd8d10f10dd8c8f4d7c5189d13dcf
LucasFerreiraDaSilva <you@example.com>
2021-12-14 22:34:13 -0300

    aprendendo js com alura

--- teste.js
+++ teste.js
@@ -1,2 +1,7 @@
-minhaLet = "eu sou um texto";
-console.log(minhaLet);
+const nome = "Lucas";
+const idade= 25tA ;
+const bebidaMaior = "Cerveja"
+const bebidaMenor = "Suco"
+
+const pedido =`${nome} Diz: "Por Favor, Quero beber ${idade >=18 ? bebidaMaior : bebidaMenor }"`
+console.log(pedido)
